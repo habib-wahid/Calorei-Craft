369949c8c9834718deb3dadbb144b9a1d5e63b39
habib_wahid <habib11wahid@gmail.com>
2024-05-04 21:06:33 +0600

    Calorie craft project set up

---
+++ src/main/java/com/example/caloriecraft/CalorieCraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.caloriecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCraftApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CalorieCraftApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/caloriecraft/CalorieCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caloriecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieCraftApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
