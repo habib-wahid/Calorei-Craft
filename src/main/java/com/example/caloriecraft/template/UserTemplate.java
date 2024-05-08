7c58bd89c3f6f65889e9f097f49a84d404d3ea0c
habib_wahid <habib11wahid@gmail.com>
2024-05-08 22:35:34 +0600

    get request chapter

---
+++ src/main/java/com/example/caloriecraft/template/UserTemplate.java
@@ -0,0 +1,23 @@
+package com.example.caloriecraft.template;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserTemplate {
+  private String id;
+  private String name;
+  private String address;
+
+//  UserTemplate(String id, String name, String address){
+//    this.id = id;
+//    this.name = name;
+//    this.address = address;
+//  }
+
+}
